231ab52c594e43731290d7c62f798143852e4cc6
Christian <christianday954@ymail.com>
2023-03-03 14:42:59 -0800

    Finish 01

--- movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -149,6 +149,10 @@ def directed_by_one_of(them)
   # Find the id and title of all the movies directed by one of 'them'.
 
   # Note: Directors appear in the 'actors' table.
+  Movie 
+    .select(:id, :title)
+    .joins(:director)
+    .where('name IN (?)', them)
 
 end
 
@@ -163,5 +167,5 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie.where('yr < 1940').pluck(:title)
 end
--- movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -1,3 +1,81 @@
+# Table name: actors
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+
+
+# class Actor < ApplicationRecord
+#   validates :name, presence: true
+
+#   has_many :castings,
+#     class_name: :Casting,
+#     foreign_key: :actor_id,
+#     primary_key: :id,
+#     dependent: :destroy
+
+#   has_many :movies,
+#     through: :castings,
+#     source: :movie
+
+#   has_many :directed_movies,
+#     class_name: :Movie,
+#     foreign_key: :director_id,
+#     primary_key: :id,
+#     dependent: :nullify
+#   end
+
+#-------------------------------------------------------------------------------------
+
+# Table name: castings
+#
+#  id         :bigint           not null, primary key
+#  actor_id   :bigint           not null
+#  movie_id   :bigint           not null
+#  ord        :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+
+# class Casting < ApplicationRecord
+#   belongs_to :actor
+#   belongs_to :movie
+# end
+
+#-------------------------------------------------------------------------------------
+
+# Table name: movies
+#
+#  id          :bigint           not null, primary key
+#  title       :string           not null
+#  yr          :integer          not null
+#  score       :float            not null
+#  votes       :integer          not null
+#  director_id :bigint
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# class Movie < ApplicationRecord
+#   validates :title, :yr, :score, :votes, presence: true
+
+#   belongs_to :director,
+#     class_name: :Actor,
+#     foreign_key: :director_id,
+#     primary_key: :id
+
+#   has_many :castings,
+#     class_name: :Casting,
+#     foreign_key: :movie_id,
+#     primary_key: :id,
+#     dependent: :destroy
+
+#   has_many :actors,
+#     through: :castings,
+#     source: :actor
+# end
+
+#-------------------------------------------------------------------------------------
+
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
