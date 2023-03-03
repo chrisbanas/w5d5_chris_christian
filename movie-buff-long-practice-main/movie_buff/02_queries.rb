494123a1a9bbda815a7283ed6121cafdbef5590c
chrisbanas <bananas595@gmail.com>
2023-03-03 15:29:11 -0800

    on vanity

--- movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -79,18 +79,32 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
-  
+
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where('yr BETWEEN 1980 AND 1989')
+    .where('score BETWEEN 3 AND 5')
+
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
-  
+  Movie
+  .group(:yr)
+  .having('MAX(score) < 8')
+  # .where.not('score > 8')
+  .pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+  Movie
+    .select('actors.id, actors.name')
+    .joins(:actors)
+    .order('ord ASC')
+    .where('title IN (?)', title)
+
 end
 
 def vanity_projects
@@ -98,11 +112,11 @@ def vanity_projects
   # starring actor. Show the movie id, title, and director's name.
 
   # Note: Directors appear in the 'actors' table.
-  
+
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name, and number of supporting roles.
-  
+
 end
